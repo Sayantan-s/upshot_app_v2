3de7bafa873f5d2202b71de3a9fe5e3ecc2bdb1b
Sayantan Samanta <sssamanta789@gmail.com>
2023-12-03 01:46:17 +0530

    feat: crop info config

--- apps/api/src/controller/media/index.ts
+++ apps/api/src/controller/media/index.ts
@@ -23,19 +23,20 @@ export class MediaController {
       Cloudinary.client.uploader.upload(file.path, {
         folder: `${type}/raw`,
       }),
-      Cloudinary.client.uploader.upload(image.filePath, {
-        folder: `${type}/cropped`,
-      }),
+      // Cloudinary.client.uploader.upload(image.filePath, {
+      //   folder: `${type}/cropped`,
+      // }),
     ]);
-    const [rawImage, croppedImage] = await uploadCloudinaryImages;
+    // const [rawImage, croppedImage] = await uploadCloudinaryImages;
 
-    await image.destroy();
+    const [rawImage] = await uploadCloudinaryImages;
+    // await image.destroy();
 
     H.success(res, {
       statusCode: 200,
       data: {
         rawImageUrl: rawImage.secure_url,
-        croppedImageUrl: croppedImage.secure_url,
+        // croppedImageUrl: croppedImage.secure_url,
       },
     });
   };
--- apps/api/src/services/media/ImageService.ts
+++ apps/api/src/services/media/ImageService.ts
@@ -1,7 +1,4 @@
-import { randomUUID } from 'crypto';
 import multer from 'multer';
-import { unlink } from 'node:fs/promises';
-import sharp from 'sharp';
 import { ICropInput } from './type';
 
 export class ImageService {
@@ -25,29 +22,29 @@ export class ImageService {
       height: metaData.height,
     };
 
-    const image = sharp(file.path);
-    let imageMetaData = await image.metadata();
+    // const image = sharp(file.path);
+    // let imageMetaData = await image.metadata();
 
-    if (config.rotate) {
-      await image.rotate(config.rotate);
-      imageMetaData = await sharp(await image.toBuffer()).metadata();
-    }
+    // if (config.rotate) {
+    //   await image.rotate(config.rotate);
+    //   imageMetaData = await sharp(await image.toBuffer()).metadata();
+    // }
 
-    cropInfo.top = Math.round((cropInfo.top / 100) * imageMetaData.height!);
-    cropInfo.left = Math.round((cropInfo.left / 100) * imageMetaData.width!);
-    cropInfo.width = Math.round((cropInfo.width / 100) * imageMetaData.width!);
-    cropInfo.height = Math.round(
-      (cropInfo.height / 100) * imageMetaData.height!
-    );
+    // cropInfo.top = Math.round((cropInfo.top / 100) * imageMetaData.height!);
+    // cropInfo.left = Math.round((cropInfo.left / 100) * imageMetaData.width!);
+    // cropInfo.width = Math.round((cropInfo.width / 100) * imageMetaData.width!);
+    // cropInfo.height = Math.round(
+    //   (cropInfo.height / 100) * imageMetaData.height!
+    // );
 
-    const uniqueCropImageId = randomUUID();
-    const croppedImagePath = `temp/images/${uniqueCropImageId}.webp`;
+    // const uniqueCropImageId = randomUUID();
+    // const croppedImagePath = `temp/images/${uniqueCropImageId}.webp`;
 
-    await image.extract(cropInfo).jpeg().toBuffer();
+    // await image.extract(cropInfo).jpeg().toBuffer();
 
     return {
-      filePath: croppedImagePath,
-      destroy: async () => await unlink(croppedImagePath),
+      // filePath: croppedImagePath,
+      // destroy: async () => await unlink(croppedImagePath),
     };
   }
 }
