8a2080cfdb11493d388775c3f6dcb3d9a5dee765
s-deep3k <sankhadeep.genx@gmail.com>
2024-03-07 22:52:16 +0530

    feat: Swagger YAML file created and executed with one route.

--- apps/api/src/middlewares/swagger.ts
+++ apps/api/src/middlewares/swagger.ts
@@ -1,13 +1,24 @@
 import { Swagger } from '@api/integrations/swagger';
 import express, { Request, Response } from 'express';
+import YAML from 'yamljs';
+import path from 'path';
 
 const swaggerMiddleware = express.Router();
+const swaggerPath = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  'apps',
+  'api',
+);
+const swaggerDocument = YAML.load(swaggerPath+"/swagger-config.yaml");
 
 //Swagger Page
 swaggerMiddleware.use(
   '/docs',
   Swagger.client.ui.serve,
-  Swagger.client.ui.setup(Swagger.client.spec)
+  Swagger.client.ui.setup(swaggerDocument)
 );
 
 //Docs in JSON format
