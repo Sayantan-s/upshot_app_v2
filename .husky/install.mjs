d8992a273ffd781ae8117c18053cbaccb8ec3a35
Sayantan Samanta <sssamanta789@gmail.com>
2024-03-19 23:18:25 +0530

    feat(scripts): added changes

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,7 +1,6 @@
 (async function () {
-  if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+  if (process.env.NODE_ENV === 'production' || process.env.CI === 'true')
     process.exit(0);
-  }
   const husky = (await import('husky')).default;
   console.log(husky());
 })();
