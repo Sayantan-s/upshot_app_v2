c775b11efc77b4c1dfaffa93d7a7e0fe1d95f416
s-deep3k <sankhadeep.genx@gmail.com>
2024-03-26 20:44:14 +0530

    feat: built the landing page using astro and email needs to be saved in mongodb(pending)

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/web',
   server: {
     open: true,
   },
+  integrations:[react(), tailwind()]
 });
