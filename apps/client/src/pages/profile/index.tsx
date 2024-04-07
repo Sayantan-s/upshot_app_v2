919d9a4770e319404c384327150567dd2de812f3
s-deep3k <sankhadeep.genx@gmail.com>
2024-04-07 17:03:43 +0530

    feat: added tabs for products and shots

--- apps/client/src/pages/profile/index.tsx
+++ apps/client/src/pages/profile/index.tsx
@@ -1,9 +1,11 @@
+import { ProfileTabs } from '@client/components/ui/Tabs';
 import { Outlet } from 'react-router-dom';
 
 export const ProfileLayout = () => {
   return (
     <div className="flex-1 bg-red-100">
       <div>Profile</div>
+      <ProfileTabs />
       <Outlet />
     </div>
   );
