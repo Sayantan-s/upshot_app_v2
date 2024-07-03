ccaeed724490269a00cbe151884ee99fd4bd3147
Sayantan Samanta <sssamanta789@gmail.com>
2024-07-03 19:49:07 +0530

    feat: added changes in ci pipeline

--- apps/web/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  outDir: '../../dist/apps/web',
-  server: {
-    open: true,
-  },
-});
